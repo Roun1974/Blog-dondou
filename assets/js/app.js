d82b37fd4deea7df59d9fa850d52d4a7930d2050
Roun1974 <diaroun74@outlook.com>
2022-06-29 00:33:12 +0200

    Ajout templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,12 @@
 import '../css/app.scss';
-
+import {Dropdown} from 'bootstrap';
+document.addEventListener('DOMContentLoaded', () => {
+    new App();
+});
+enableDropdowns()
+{
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+    dropdownElementList.map(function (dropdownToggleEl) {
+        return new Dropdown(dropdownToggleEl);
+    });
+}
