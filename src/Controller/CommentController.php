e691ff70022881e499112be8f9258f4aa04c0089
Roun1974 <diaroun74@outlook.com>
2022-07-03 23:36:16 +0200

    Authentification

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -14,6 +14,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/** @method User getUser() */
 class CommentController extends AbstractController
 {
     #[Route('/ajax/comments', name: 'comment_add', methods: ['POST'])]
@@ -31,9 +32,17 @@ class CommentController extends AbstractController
                 'code' => 'ARTICLE_NOT_FOUND'
             ], Response::HTTP_BAD_REQUEST);
         }
+        $user = $this->getUser();
+        
+        if($user){
+          return $this->json([
+            'code' => 'USER_NOT_AUTHENTICATED_FULLY'
+        ], Response::HTTP_BAD_REQUEST);
+        }
+
         $comment= new Comment($article);
         $comment->setContent($commentData['content']);
-        $comment->setUser($userRepo->findOneBy(['id'=>1]));
+        $comment->setUser($user);
         $comment->setCreatedAt(new \Datetime());
         $comment->handleRequest($request);
         
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('username', TextType::class, [
+            'label' => "Nom d'utilisateur :"
+        ])
+           
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'label' => 'Mot de passe',
+                'options' => [
+                    'attr' => [
+                        'type' => 'password'
+                    ]
+                ],
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 6
+                    ])
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => "S'inscrire"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
